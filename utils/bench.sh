2a35721c6437e26fac8a752eff1b325550f966ae
Shengyu Zhang <i@silverrainz.me>
2025-10-05 23:46:42 +0800

    chore: Improve bench script (again)

--- utils/bench.sh
+++ utils/bench.sh
@@ -3,42 +3,55 @@
 # set -x
 set -e
 
-sphinxver=v7.3.7
-sphinxopts=-Q
-mktarget=$(tail -n+25 $(git rev-parse --show-toplevel)/docs/Makefile)
+sphinx_ver=v8.2.3
+sphinx_opts=-Q
+mk_target=$(tail -n+25 $(git rev-parse --show-toplevel)/docs/Makefile)
 
-tmpdir=$(mktemp -d)
+pydoc_url=https://github.com/python/cpython.git
+pydoc_ver=v3.13.3
+pydoc_repo=cpython
+pydoc_path=Doc
+
+tmpdir=/tmp/sphinxnotes-fasthtml-bench
 echo "tmpdir: $tmpdir"
-cd $tmpdir
+if [ -z "$USE_CACHE" ]; then
+    rm -rf $tmpdir || true
+    mkdir -p $tmpdir || true
+    cd $tmpdir
+    git clone --quiet --branch $pydoc_ver --depth 1 $pydoc_url
+    cd $pydoc_repo/$pydoc_path
+    make venv
+else
+    cd $tmpdir/$pydoc_repo
+    git restore .
+    git clean -fd .
+    cd $pydoc_path
+fi
+
+source ./venv/bin/activate
 
 python --version
-python -m venv .
-source ./bin/activate
-
+python -m pip install sphinx==$sphinx_ver sphinxnotes-fasthtml >/dev/null
 pip_version() {
     python -m pip show $1 | grep Version | cut -d ' ' -f2
 }
-
-python -m pip install sphinx==$sphinxver sphinxnotes-fasthtml >/dev/null
 echo "Sphinx: $(pip_version sphinx)"
 echo "sphinxnotes-fasthtml: $(pip_version sphinxnotes-fasthtml)"
 
 compare() {
     touch index.rst
-    make SPHINXOPTS=$sphinxopts html
+    make SPHINXOPTS=$sphinx_opts html
     echo ">>> Standard build"
     touch index.rst
-    time make SPHINXOPTS=$sphinxopts html
+    time make SPHINXOPTS=$sphinx_opts html
     echo ">>> Fast build"
     touch index.rst
-    time make SPHINXOPTS=$sphinxopts fasthtml
+    time make SPHINXOPTS=$sphinx_opts fasthtml
 }
 
-git clone --quiet --branch $sphinxver --depth 1 https://github.com/sphinx-doc/sphinx.git 2>/dev/null
-cd sphinx/doc
 echo "extensions.append('sphinxnotes.fasthtml')" >> conf.py
-echo "$mktarget" >> Makefile
+echo "$mk_target" >> Makefile
 compare
 cd ..
 
-rm -rf $tmpdir
+# rm -rf $tmpdir
